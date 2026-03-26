1be7c481e429638b2fcc0334df20c167938440fb
vaishnavisidduluri <vaishnavisidduluri@gmail.com>
2026-03-26 10:48:11 +0530

    added object detection logic in vision module

---
+++ perception/vision/object_detector.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+
+class ObjectDetector:
+    def __init__(self, model_path="yolov8n.pt"):
+        self.model = YOLO(model_path)
+
+    def detect(self, frame):
+        results = self.model(frame)[0]
+        detections = []
+
+        for box in results.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            confidence = float(box.conf[0])
+            class_id = int(box.cls[0])
+            label = self.model.names[class_id]
+
+            center_x = int((x1 + x2) / 2)
+            center_y = int((y1 + y2) / 2)
+
+            detections.append({
+                "id": class_id,
+                "label": label,
+                "confidence": round(confidence, 2),
+                "bbox": [x1, y1, x2, y2],
+                "center": [center_x, center_y]
+            })
+
+        return detections
